bf9da63dc6cff0d701d49645625dd0bc68898f40
Tapishagrawal <tapishg4@gmail.com>
2023-10-24 01:30:52 +0530

    change in nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,7 +35,7 @@ export default function Header() {
                 animate={{ y: 0, x: "-50%", opacity: 1 }}
             ></motion.div>
             <nav className='flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
-                <ul className='flex w-[22rem] items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial]  sm:flex-nowrap sm:gap-10'>
+                <ul className='flex flex-wrap w-[22rem] items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial]  sm:flex-nowrap sm:gap-10'>
                     {links.map(link => (
                         <motion.li className='h-3/4 flex items-center justify-center relative'
                             key={link.hash}
