4fb2e58232057f878036e6e4cf312c09c3a1d519
Tapishagrawal <tapishg4@gmail.com>
2023-11-05 13:12:43 +0530

    github calenders id

--- src/components/GitHubCalendarSec.jsx
+++ src/components/GitHubCalendarSec.jsx
@@ -6,15 +6,15 @@ export const GitHubCalendarSec = () => {
             <div className='react-activity-calendar flex justify-center items-start'>
                 <GitHubCalendar username="tapishagrawal" colorScheme="light" />
             </div>
-            <div id="github-top-langs" className="flex justify-center items-start">
-                <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=tapishagrawal&layout=compact" />
+            <div className="flex justify-center items-start">
+                <img id="github-top-langs" src="https://github-readme-stats.vercel.app/api/top-langs/?username=tapishagrawal&layout=compact" />
             </div>
             <div className="flex justify-center flex-col items-center gap-8 sm:flex-row">
-                <div id="github-streak-stats" className="flex justify-center items-start">
-                    <img src="https://github-readme-streak-stats.herokuapp.com?user=tapishagrawal&theme=dark&hide_border=true&border_radius=4.8" alt="GitHub Streak" />
+                <div className="flex justify-center items-start">
+                    <img id="github-streak-stats" src="https://github-readme-streak-stats.herokuapp.com?user=tapishagrawal&theme=dark&hide_border=true&border_radius=4.8" alt="GitHub Streak" />
                 </div>
                 <div id="github-stats-card">
-                    <img src="https://github-readme-stats.vercel.app/api?username=anuraghazra&show_icons=true&theme=tokyonight" />
+                    <img id="github-streak-stats" src="https://github-readme-stats.vercel.app/api?username=anuraghazra&show_icons=true&theme=tokyonight" />
                 </div>
             </div>
         </div>
